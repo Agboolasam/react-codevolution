e4e001ad6c2d02393a5e90c4e6e511877e54e7eb
TheRealAGBOOLA <agboolasamson602@gmail.com>
2023-11-17 17:01:17 +0100

    render my first html

--- my-first-app/src/App.js
+++ my-first-app/src/App.js
@@ -1,25 +1,11 @@
 import logo from "./logo.svg";
 import "./App.css";
-import sayHello from "./components/greet.js";
+import Hello from "./components/Greet"; //component name to be rendered must start with uppercase
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code> src / App.js </code> and save to reload.{" "}
-        </p>{" "}
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React{" "}
-        </a>{" "}
-      </header>{" "}
-      <sayHello />
+      <Hello />
     </div>
   );
 }
--- my-first-app/src/components/greet.js
+++ my-first-app/src/components/greet.js
@@ -1,8 +1,6 @@
 //first thing to do is to import react
 import React from "react";
 
-function sayHello() {
-  return <h1> Hello Agboola </h1>;
-}
+const Hello = () => <h1> hello agboola </h1>;
 
-export default sayHello;
+export default Hello;
