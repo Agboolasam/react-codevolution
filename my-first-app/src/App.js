649233b538453f5160aac9440fcf31fb489db431
TheRealAGBOOLA <agboolasamson602@gmail.com>
2023-11-17 16:20:08 +0100

    added greet.js module

--- my-first-app/src/App.js
+++ my-first-app/src/App.js
@@ -1,5 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import sayHello from "./components/greet.js";
 
 function App() {
   return (
@@ -7,17 +8,18 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+          Edit <code> src / App.js </code> and save to reload.{" "}
+        </p>{" "}
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
-        </a>
-      </header>
+          Learn React{" "}
+        </a>{" "}
+      </header>{" "}
+      <sayHello />
     </div>
   );
 }
---
+++ my-first-app/src/components/greet.js
@@ -0,0 +1,8 @@
+//first thing to do is to import react
+import React from "react";
+
+function sayHello() {
+  return <h1> Hello Agboola </h1>;
+}
+
+export default sayHello;
